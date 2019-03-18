cb9e3bd9db382fee6e1b94bf2faa89047daeab29
phlimasg <raphaelpc_@hotmail.com>
2019-03-18 16:37:00 -0300

    grafico dashboard
    grafico dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Model\totvs;
 use App\Model\UVW_STE_ALUNOS_E_RESPONSAVEIS;
+use App\Model\inscricao;
 
 class HomeController extends Controller
 {
@@ -29,6 +30,12 @@ class HomeController extends Controller
         ->whereNotIn('RA', totvs::select('RA')->orderBy('RA')->limit(2000,2000)->get())
         ->get();        
         dd(totvs::select('RA')->orderBy('RA')->limit(2000,2000)->get());     */
+        $grafico = inscricao::join('atv_extra_turmas','atv_extra_turma_id','atv_extra_turmas.id')
+        ->join('atv_extras','atv_extra_id','atv_extras.id')
+        ->selectRaw('descricao_atv, count(*) as insc')
+        ->groupBy('atv_extras.id')
+        ->orderBy('descricao_atv')
+        ->get();
         $count1 = UVW_STE_ALUNOS_E_RESPONSAVEIS::count();
         $count2 = totvs::count();        
         if ($count1 > $count2) {
@@ -55,8 +62,8 @@ class HomeController extends Controller
                 $local->save();
             }
             $up = 'Base de alunos atualizada.';
-            return view('home',compact('up'));
+            return view('home',compact('up','grafico'));
         }        
-        return view('home');
+        return view('home',compact('grafico'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,5 +10,62 @@
             {{$up}}
         </div>       
     @endif
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">   
+           
+
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawBarColors);
+
+function drawBarColors() {
+      var data = google.visualization.arrayToDataTable([
+        
+        ['Atividades',''],
+        @foreach($grafico as $g)
+          ['{{$g->descricao_atv}}', {{$g->insc}}],
+        @endforeach        
+      ]);
+
+      var options = {
+        title: 'Inscrições - Atividades Extraclasse',
+        chartArea: {width: '50%'},
+        colors: ['#193c72'],
+        hAxis: {
+          title: 'Total das inscrições',
+          minValue: 0
+        },
+        vAxis: {
+          title: 'Atividades'
+        }
+      };
+      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
+
+    </script>
+    <div class="row">
+      <div id="chart_div" class="col-sm-8" style="height: 500px;"></div>
+      <div class="col-sm-4">
+          <div class="table-responsive">          
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Atividade</th>
+                    <th>Quantidade</th>                   
+                  </tr>
+                </thead>
+                <tbody>
+                  
+                    @foreach($grafico as $g)
+                    <tr>
+                      <td>{{$g->descricao_atv}}</td>
+                      <td>{{$g->insc}}</td>
+                    </tr>
+                    @endforeach                      
+                  
+                </tbody>
+              </table>
+      </div>
+    </div>
 </div>
 @endsection
